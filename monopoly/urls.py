bb7e1a223a2fb27fdb0a10db952764319b009e1d
Atanas Penchev <atanas.penchev@outlook.com>
2015-03-01 16:27:43 +0000

    Initial game creation logic.

---
+++ monopoly/urls.py
@@ -0,0 +1,7 @@
+from django.conf.urls import patterns, url
+from monopoly import views
+
+urlpatterns = patterns('',
+        url(r'^$', views.index, name='index'),
+        url(r'^new_game/(?P<private>[\w\-]+)/$', views.new_game, name='new_game'),
+)
--- monopoly/views.py
+++ monopoly/views.py
@@ -1,3 +1,19 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.http import HttpResponse
+
+from monopoly.models import Game
 
 # Create your views here.
+def index(request):
+	return HttpResponse('Hello')
+
+def new_game(request, private):
+	#private = request.GET['private'] or False
+	print private
+
+	game = Game()
+	game.private = True if private == "private" else False
+	game.save()
+	print game
+
+	return redirect(index)
