da75f7f926ca1d553b7d07e3727f87beb60966b8
Atanas Penchev <atanas.penchev@outlook.com>
2015-03-01 18:36:23 +0000

    Initializing squares at game creation.

---
+++ monopoly/board.py
@@ -0,0 +1,169 @@
+properties = [
+    {
+        'type': 'property',
+        'position': 1,
+        'title': 'Old Kent Toad'
+    },
+    {
+        'position': 3,
+        'title': 'Whitechapel Road'
+    },
+    {
+        'position': 6,
+        'title': 'The Angel Inslington'
+    },
+    {
+        'position': 8,
+        'title': 'Euston Road'
+    },
+    {
+        'position': 9,
+        'title': 'Penton View Road'
+    },
+    {
+        'position': 11,
+        'title': 'Pall Mall'
+    },
+    {
+        'position': 13,
+        'title': 'White Hall'
+    },
+    {
+        'position': 14,
+        'title': 'Norton Avenue'
+    },
+    {
+        'position': 16,
+        'title': 'Bow Street'
+    },
+    {
+        'position': 18,
+        'title': 'Marlboro Street'
+    },
+    {
+        'position': 19,
+        'title': 'Vine Street'
+    },
+    {
+        'position': 21,
+        'title': 'Strand'
+    },
+    {
+        'position': 23,
+        'title': 'Fleet Street'
+    },
+    {
+        'position': 24,
+        'title': 'Trafalgar Square'
+    },
+    {
+        'position': 26,
+        'title': 'Lester Square'
+    },
+    {
+        'position': 27,
+        'title': 'Coventry Street'
+    },
+    {
+        'position': 29,
+        'title': 'Picadilly'
+    },
+    {
+        'position': 31,
+        'title': 'Regent Street'
+    },
+    {
+        'position': 32,
+        'title': 'Oxford Street'
+    },
+    {
+        'position': 34,
+        'title': 'Bond Street'
+    },
+    {
+        'position': 37,
+        'title': 'Park Lane'
+    },
+    {
+        'position': 39,
+        'title': 'Mayfair'
+    }
+]
+
+utilities = [
+    {
+        'position': 5,
+        'title': 'Kings Cross Station'
+    },
+    {
+        'position': 12,
+        'title': 'Electric Company'
+    },
+    {
+        'position': 15,
+        'title': 'Maryll Boan Station'
+    },
+    {
+        'position': 25,
+        'title': 'Fenchurchstreet Station'
+    },
+    {
+        'position': 28,
+        'title': 'Waterworks'
+    },
+    {
+        'position': 35,
+        'title': 'Liverpoolstreet Station'
+    }
+]
+
+specials = [
+    {
+        'position': 0,
+        'title': 'Go'
+    },
+    {
+        'position': 2,
+        'title': 'Community Chest'
+    },
+    {
+        'position': 4,
+        'title': 'Income Tax'
+    },
+    {
+        'position': 7,
+        'title': 'Chance'
+    },
+    {
+        'position': 10,
+        'title': 'Jail'
+    },
+    {
+        'position': 17,
+        'title': 'Community Chest'
+    },
+    {
+        'position': 20,
+        'title': 'Free Parking'
+    },
+    {
+        'position': 22,
+        'title': 'Chance'
+    },
+    {
+        'position': 30,
+        'title': 'Go to Jail'
+    },
+    {
+        'position': 33,
+        'title': 'Community Chest'
+    },
+    {
+        'position': 36,
+        'title': 'Chance'
+    },
+    {
+        'position': 38,
+        'title': 'Supertax'
+    }
+]
--- monopoly/views.py
+++ monopoly/views.py
@@ -1,7 +1,8 @@
 from django.shortcuts import render, redirect
 from django.http import HttpResponse
 
-from monopoly.models import Game
+from monopoly.models import Game, Square, Property, Utility, Special
+from board import properties, utilities, specials
 
 # Create your views here.
 def index(request):
@@ -9,12 +10,42 @@ def index(request):
 
 def game(request, id):
     game = Game.objects.get(id=id)
-    print game
+
     return HttpResponse(str(game.id) + ' ' + str(game.private))
 
 def new_game(request, private):
     newGame = Game()
     newGame.private = True if private == "private" else False
     newGame.save()
+
+    squares = []
+    for x in range(40):
+        square = Square(position=x, game=newGame, title='Square ' + str(x))
+        square.save()
+        squares.append(square)
+
+    for prop in properties:
+        property = Property()
+        property.square = squares[prop['position']]
+        property.square.title = prop['title']
+        property.square.save()
+        property.save()
+
+    for util in utilities:
+        utility = Utility()
+        utility.square = squares[util['position']]
+        utility.square.title = util['title']
+        utility.square.save()
+        utility.save()
+
+    for spec in specials:
+        special = Special()
+        special.square = squares[spec['position']]
+        special.square.title = spec['title']
+        special.square.save()
+        special.save()
+
+    for square in squares:
+        print square.title
 
     return redirect(game, id=newGame.id)
