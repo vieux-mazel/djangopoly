82f2e5ff18630adc5703ca883763ee80892455f5
Atanas Penchev <atanas.penchev@outlook.com>
2015-03-08 16:15:20 +0000

    End turn

--- monopoly/urls.py
+++ monopoly/urls.py
@@ -7,4 +7,5 @@ urlpatterns = patterns('',
         url(r'^game/roll/$', views.roll_dice, name='roll_dice'),
         url(r'^game/(?P<id>[\w\-]+)/$', views.game, name='game'),
         url(r'^game/(?P<id>[\w\-]+)/start/$', views.start_game, name='start_game'),
+        url(r'^game/(?P<id>[\w\-]+)/end_turn/$', views.end_turn, name='end_turn'),
 )
