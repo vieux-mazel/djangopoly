2bc9212ac79fd5620ac45d2fd42e741456200973
Didier Raboud <oriole@vieux-mazel.ch>
2016-04-01 16:59:52 +0200

    Ajoute les objets par défaut dans l'admin

--- monopoly/admin.py
+++ monopoly/admin.py
@@ -1,6 +1,15 @@
 from django.contrib import admin
-from .models import UserProfile
 
-
-admin.site.register(UserProfile)
 # Register your models here.
+
+from .models import (
+    Game, Square, Player, Street, Utility, Effect, Special
+)
+
+admin.site.register(Game)
+admin.site.register(Square)
+admin.site.register(Player)
+admin.site.register(Street)
+admin.site.register(Utility)
+admin.site.register(Effect)
+admin.site.register(Special)
