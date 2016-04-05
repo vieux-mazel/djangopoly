dcbd6c9f1d5d4c1463e4565b36ca730b44b8b262
Didier Raboud <oriole@vieux-mazel.ch>
2016-04-05 21:08:21 +0200

    force import_env_vars

--- djangopoly/wsgi.py
+++ djangopoly/wsgi.py
@@ -8,9 +8,22 @@ https://docs.djangoproject.com/en/1.7/howto/deployment/wsgi/
 """
 
 import os
+import glob
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "djangopoly.settings")
 
-from djangopoly import import_env_vars
+def import_env_vars(directory):
+    """
+    List the files present in the given directory and for each of them create
+    an environment variable named after the file, and which value is the
+    contents of the file.
+    """
+    env_vars = glob.glob(os.path.join(directory, '*'))
+
+    for env_var in env_vars:
+        with open(env_var, 'r') as env_var_file:
+            os.environ.setdefault(env_var.split(os.sep)[-1],
+                                  env_var_file.read().strip())
+
 import_env_vars(os.path.join(os.path.dirname(os.path.dirname(__file__)), 'env'))
 
 from django.core.wsgi import get_wsgi_application
